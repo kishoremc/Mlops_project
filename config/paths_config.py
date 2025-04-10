bc0e4a9e02c85b5131ea565578c8d6e9d090c03a
kishore <kishorem.chandru@gmail.com>
2025-04-10 22:11:16 +0530

    model training and mlflow

--- config/paths_config.py
+++ config/paths_config.py
@@ -10,3 +10,6 @@ PROCESSED_DATA_PATH = os.path.join(ARTIFACTS_DIR,"processed_data","processed_tra
 
 ENGINEERED_DIR = os.path.join(ARTIFACTS_DIR,"engineered_data")
 ENGINEERED_DATA_PATH = os.path.join(ARTIFACTS_DIR,"engineered_data","final_df.csv")
+
+PARAMS_PATH = os.path.join("./config" , "params.json")
+MODEL_SAVE_PATH = os.path.join(ARTIFACTS_DIR,"models" , "trained_model.pkl")
